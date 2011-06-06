8fa0253ae84f54b934522fc6030f2b8465c96e4f
Julien Ammous <j.ammous@gmail.com>
2011-06-06 22:38:10 +0200

    some reorganization

--- src/event_handlers/dummy/collector_dummy_handler.erl
+++ src/event_handlers/dummy/collector_dummy_handler.erl
@@ -4,6 +4,8 @@
 
 -behaviour(gen_event).
 
+-include("collector.hrl").
+
 %% gen_event callbacks
 -export([init/1, handle_event/2, handle_call/2, handle_info/2, terminate/2, code_change/3]).
 
@@ -11,8 +13,9 @@ init([]) ->
   io:format("Values Handler installed.~n", []),
   {ok, nostate}.
 
-handle_event({activity, Event}, State) ->
-  io:format("Event: ~w~n", [Event]),
+handle_event({activity, Event = {activity, #value_event{host = H, time = T}}}, State) ->
+  io:format("Activity:~n  Host: ~s~n  Time: ~w~n  Plugin: ~s~n  Type: ~s~n~n", [H, T, Event#value_event.plugin, Event#value_event.type]),
+  % io:format("Event: ~w~n", [Event]),
   {ok, State}.
 
 
